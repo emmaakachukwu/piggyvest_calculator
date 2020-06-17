29c4477f7e846e19c863701405d69deb4ef9f981
= <=>
2020-06-18 00:45:35 +0100

    started collection of inputs for posting

--- public/script.js
+++ public/script.js
@@ -1,16 +1,31 @@
-let amount = document.getElementById('amount');
+let amount = document.getElementById('amount').value;
 let investment = document.getElementById('investment').value;
 let rate = document.getElementById('rate').value;
-let time = document.getElementById('time').value;
+let period = document.getElementById('period').value;
+let interestCalc = document.getElementById('interest');
 let submit = document.getElementById('btn');
 
 submit.addEventListener('click', onSubmit);
 
-function onSubmit(){
+async function onSubmit(){
     let error;
     if ( !amount || !investment || !rate || !time ) {
         error = "All fields are requitred";
     } else {
-        //continue here
+        let interest = (amount * rate * period) / 100;
+        interestCalc.value = interest;
+        return;
+        const postInterest = await fetch( '/', { //set the url to post the inputs
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                amount,
+                investment,
+                interest
+            })
+        })
+        console.log(postInterest.json()); //response returns here
     }
 }
