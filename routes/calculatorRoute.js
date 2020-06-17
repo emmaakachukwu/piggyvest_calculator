ec68574838176fd92113e0357c2cae3fb2895d39
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-17 22:21:04 +0100

    cleaning up

--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -5,19 +5,19 @@ const calculator_controller = require("../controllers/calculatorController");
 
 
 //GET request for the calculator homepage
-router.get("/", calculator_controller.index);
+router.get("/", calculator_controller.getIndex);
 
 //GET request for calculated interest
-router.get("/interest/calculate", calculator_controller.interest_calculate_get);
+// router.get("/interest/calculate", calculator_controller.interest_calculate_get);
 
 //POST request for calculating interest
-router.post("/interest/calculate", calculator_controller.interest_calculate_post);
+router.post("/interest/calculate", calculator_controller.postInterest);
 
 //POST for deleting an interest
-router.post("/interest/:id/delete", calculator_controller.interest_delete_post);
+// router.post("/interest/:id/delete", calculator_controller.interest_delete_post);
 
 //GET request for a deleted interest
-router.get("/interest/:id/delete", calculator_controller.interest_delete_get);
+// router.get("/interest/:id/delete", calculator_controller.interest_delete_get);
 
 
 module.exports = router;
