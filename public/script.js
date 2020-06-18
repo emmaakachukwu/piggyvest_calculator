a5ad46555bdb49c8c6ed43c52a48b33143caf268
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-18 09:11:52 +0100

    fixed calculator

--- public/script.js
+++ public/script.js
@@ -1,30 +1,29 @@
-let amount = document.getElementById('amount');
-let investment = document.getElementById('investment');
-let rate = document.getElementById('rate');
-let period = document.getElementById('period');
-let interestCalc = document.getElementById('interest');
-let submit = document.getElementById('btn');
+function calculateTotal() {
+  var month = document.myform.period.value;
+  var amount = document.myform.amount.value;
+  var r = 10;
+  var totalInterest = (r * amount * month) / 100;
+  var m = parseInt(month);
+  var i = parseInt(totalInterest);
+  var l = parseInt(amount);
 
-submit.addEventListener('click', onSubmit);
-async function onSubmit(){
-    let error;
-    if ( !amount.value || !investment.value || !rate.value || !period.value ) {
-        error = "All fields are requitred";
-    } else {
-        let interest = (parseInt(amount.value) * parseInt(rate.value) * parseInt(period.value)) / 100;
-        interestCalc.innerHTML = `<span>&#8358;${interest}</span>`;
-        return;
-        const postInterest = await fetch( '/', { //set the url to post the inputs
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                amount: amount.value,
-                investment: investment.value,
-                interest
-            })
-        })
-        console.log(postInterest.json()); //response returns here
-    }
+  totalReturn = eval(totalInterest);
+  totalTotal = eval(l + i);
+
+  document.getElementById("totalinterestrate").innerHTML = totalReturn;
 }
+
+document.getElementById("btn").onclick = function () {
+  var month = document.myform.period.value;
+  var amount = document.myform.amount.value;
+  var r = 10;
+  var totalInterest = (r * amount * month) / 100;
+
+  var m = parseInt(month);
+  var i = parseInt(totalInterest);
+  var l = parseInt(amount);
+
+  totalReturn = eval(totalInterest);
+  totalTotal = eval(l + i);
+  document.getElementById("totalinterest").innerHTML = totalTotal;
+};
