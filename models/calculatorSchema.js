826a3c37253c84ffd324461427f7fe464aadb226
= <=>
2020-06-18 07:24:31 +0100

    merged front end

--- models/calculatorSchema.js
+++ models/calculatorSchema.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const mongoose = require("mongoose");
 
 
 const calculatorSchema = new mongoose.Schema({
--- public/script.js
+++ public/script.js
@@ -6,14 +6,13 @@ let interestCalc = document.getElementById('interest');
 let submit = document.getElementById('btn');
 
 submit.addEventListener('click', onSubmit);
-
 async function onSubmit(){
     let error;
     if ( !amount.value || !investment.value || !rate.value || !period.value ) {
         error = "All fields are requitred";
     } else {
         let interest = (parseInt(amount.value) * parseInt(rate.value) * parseInt(period.value)) / 100;
-        interestCalc.innerHTML = `<span>${interest}</span>`;
+        interestCalc.innerHTML = `<span>&#8358;${interest}</span>`;
         return;
         const postInterest = await fetch( '/', { //set the url to post the inputs
             method: 'POST',
