bcc46e16f2de896c3d8e1d9881f46f4102fb4de8
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-19 10:01:24 +0100

    pdf creation

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,17 +1,30 @@
 var calculatorSchema = require("../models/calculatorSchema");
-
+const pdfk= require('pdfkit')
 //Piggyvest calculator homepage
 exports.getIndex = (req, res)=> {
     res.render("../views/index", {
-        Title: "Welcome to Co-op Lend"
       });
 };
 
 
-//List of all calculated interest
-// exports.calculatorSchema_get = function(req, res) {
-//     res.send("Lists Of All Interest");
-// };
+exports.getFile= (req,res,next)=>{
+    const pdfDoc = new Pdfk();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'inline; filename="' + piggyvest + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream('piggyvest.pdf'));
+      pdfDoc.pipe(res);
+      pdfDoc.fontSize(15).text('Wally Gator !', 50, 50);
+// Set the paragraph width and align direction
+doc.text('Wally Gator is a swinging alligator in the swamp. He\'s the greatest percolator when he really starts to romp. There has never been a greater operator in the swamp. See ya later, Wally Gator.', {
+    width: 410,
+    align: 'left'
+});
+// PDF Creation logic goes here
+doc.end();
+}
 
 
 //Calculate interest
