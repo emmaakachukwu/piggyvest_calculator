3984b6048488f474899acf3fe433110b64d34473
= <=>
2020-06-19 19:00:17 +0100

    pdfkit implemented and working

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,37 +1,28 @@
 // var calculatorSchema = require("../models/calculatorSchema");
+const PDFDocument = require('pdfkit');
 const fs = require('fs');
-const pdfk= require('pdfkit')
+
+const doc = new PDFDocument;
+
 //Piggyvest calculator homepage
 exports.getIndex = (req, res)=> {
-    res.render("../views/index", {
-      });
+    res.render("../views/index");
 };
-
 
 exports.getFile = async (req,res,next) => {
     const amount=req.body.amount
     const month=req.body.month
     const interest=req.body.interest
-    console.log(amount ,month)
-    const name = 'piggy-' + '.pdf';
-    const pdfDoc = new pdfk();
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader(
-        'Content-Disposition',
-        'inline; filename="' + name+ '"'
-      );
-      pdfDoc.pipe(fs.createWriteStream('piggyvest.pdf'));
-      pdfDoc.pipe(res);
-      pdfDoc.fontSize(15).text('Wally Gator !', 50, 50);
-// Set the paragraph width and align direction
-pdfDoc.text(amount , month, {
-    width: 310,
-    align: 'left'
-});
-// PDF Creation logic goes here
-pdfDoc.end();
+    
+    doc.pipe(fs.createWriteStream(`./exports/piggyvest-${Date.now()}.pdf`));
+    doc.text(`
+        Date: ${Date.now()} \n \n
+        Amount: ${amount} \n \n
+        Interest: ${interest} \n \n
+        In ${month} month${month > 1 ? 's' : ''} time, with continuous savings of ${amount} every month, you will have saved ${parseInt(amount) + interest } naira.
+    `);
+    doc.end()
 }
-
 
 //Calculate interest
 // exports.postInterest = (req, res)=> {
